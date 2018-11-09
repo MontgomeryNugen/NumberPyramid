902387553de85939754ac69c9129f5f2ddae644c
Montgomery Nugen <montgomery.nugen@gmail.com>
2018-11-09 11:12:05 -0500

    initial commit

---
+++ src/Pyramid.java
@@ -0,0 +1,17 @@
+
+public class Pyramid {
+
+	public static void main(String[] args) {
+
+		int number = 5;
+		
+		for (int j = number; j > 0; j--) {
+		for (int i = 1; i <= number; i++) {
+			System.out.print(number);
+		}
+			System.out.println();
+			number --;
+		}
+	}
+
+}
